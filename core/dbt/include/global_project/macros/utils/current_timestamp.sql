38d26f12decafdc065113d816b3e45dc6b7d9214
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-25 10:54:11 -0600

    Remove conflicting definition of current_timestamp()

--- core/dbt/include/global_project/macros/utils/current_timestamp.sql
+++ core/dbt/include/global_project/macros/utils/current_timestamp.sql
@@ -1,11 +1,3 @@
-{% macro current_timestamp() -%}
-  {{ return(adapter.dispatch('current_timestamp', 'dbt')()) }}
-{%- endmacro %}
-
-{% macro default__current_timestamp() %}
-    current_timestamp::{{dbt.type_timestamp()}}
-{% endmacro %}
-
 {% macro current_timestamp_in_utc() -%}
   {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt')()) }}
 {%- endmacro %}
