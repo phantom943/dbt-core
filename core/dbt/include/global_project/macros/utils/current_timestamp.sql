2670833923282755bc7e1a4903b33b262f73f06c
Doug Beatty <doug.beatty@dbtlabs.com>
2022-06-06 19:35:04 -0600

    Kick out the `current_timestamp` macros

--- core/dbt/include/global_project/macros/utils/current_timestamp.sql
+++
@@ -1,7 +0,0 @@
-{% macro current_timestamp_in_utc() -%}
-  {{ return(adapter.dispatch('current_timestamp_in_utc', 'dbt')()) }}
-{%- endmacro %}
-
-{% macro default__current_timestamp_in_utc() %}
-    {{dbt.current_timestamp()}}
-{% endmacro %}
