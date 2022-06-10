ba341e6a68d4b9cf8dc40b615efcaf5f15c38b18
Gerda Shank <gerda@dbtlabs.com>
2022-06-10 16:25:37 -0400

    Add adapter.dispatch calls to new macros

--- core/dbt/include/global_project/macros/materializations/models/incremental/strategies.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/strategies.sql
@@ -1,5 +1,12 @@
 {% macro get_incremental_append_sql(arg_dict) %}
 
+  {{ return(adapter.dispatch('get_incremental_append_sql', 'dbt')(arg_dict)) }}
+
+{% endmacro %}
+
+
+{% macro default__get_incremental_append_sql(arg_dict) %}
+
   {% do return(get_insert_into_sql(arg_dict["target_relation"], arg_dict["temp_relation"], arg_dict["dest_columns"])) %}
 
 {% endmacro %}
@@ -7,6 +14,12 @@
 
 {# snowflake #}
 {% macro get_incremental_delete_insert_sql(arg_dict) %}
+
+  {{ return(adapter.dispatch('get_incremental_delete_insert_sql', 'dbt')(arg_dict)) }}
+
+{% endmacro %}
+
+{% macro default__get_incremental_delete_insert_sql(arg_dict) %}
 
   {% do return(get_delete_insert_merge_sql(arg_dict["target_relation"], arg_dict["temp_relation"], arg_dict["unique_key"], arg_dict["dest_columns"])) %}
 
@@ -16,6 +29,12 @@
 {# snowflake, bigquery, spark #}
 {% macro get_incremental_merge_sql(arg_dict) %}
 
+  {{ return(adapter.dispatch('get_incremental_merge_sql', 'dbt')(arg_dict)) }}
+
+{% endmacro %}
+
+{% macro default__get_incremental_merge_sql(arg_dict) %}
+
   {% do return(get_merge_sql(arg_dict["target_relation"], arg_dict["temp_relation"], arg_dict["unique_key"], arg_dict["dest_columns"])) %}
 
 {% endmacro %}
@@ -23,12 +42,24 @@
 
 {% macro get_incremental_insert_overwrite_sql(arg_dict) %}
 
+  {{ return(adapter.dispatch('get_incremental_insert_overwrite_sql', 'dbt')(arg_dict)) }}
+
+{% endmacro %}
+
+{% macro default__get_incremental_insert_overwrite_sql(arg_dict) %}
+
   {% do return(get_insert_overwrite_merge_sql(arg_dict["target_relation"], arg_dict["temp_relation"], arg_dict["dest_columns"], arg_dict["predicates"])) %}
 
 {% endmacro %}
 
 
 {% macro get_incremental_default_sql(arg_dict) %}
+
+  {{ return(adapter.dispatch('get_incremental_default_sql', 'dbt')(arg_dict)) }}
+
+{% endmacro %}
+
+{% macro default__get_incremental_default_sql(arg_dict) %}
 
   {% do return(get_incremental_append_sql(arg_dict)) %}
 
