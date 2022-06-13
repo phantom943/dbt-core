5a8ad9251229e97ab1b20b2750db721c6be18fc8
Gerda Shank <gerda@dbtlabs.com>
2022-06-13 17:53:05 -0400

    Create postgres default strategy macro

--- core/dbt/include/global_project/macros/materializations/models/incremental/strategies.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/strategies.sql
@@ -61,11 +61,7 @@
 
 {% macro default__get_incremental_default_sql(arg_dict) %}
 
-  {% if arg_dict["unique_key"] %}
-    {% do return(get_incremental_delete_insert_sql(arg_dict)) %}
-  {% else %}
-    {% do return(get_incremental_append_sql(arg_dict)) %}
-  {% endif %}
+  {% do return(get_incremental_append_sql(arg_dict)) %}
 
 {% endmacro %}
 
